575b00c697b451737be1d5dc80341be6571992ca
Ben <bhawkyard1@gmail.com>
2026-05-31 18:12:02 +0100

    Locate the user on map open, and add a popup at the closest loc on the Thames.
    Next, figure out how far along the thames we
    are, and interpolate between tide times for
    nearest stations.

--- frontend/app.js
+++ frontend/app.js
@@ -4,3 +4,53 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: attribution
 }).addTo(map);
+
+
+map.locate({setView: true, maxZoom: 16});
+
+const tides_url = "http://localhost:8081";
+var popup_thames = L.popup();
+var acccircle = L.circle([0, 0], 0).addTo(map);
+
+async function onLocationFound(e) {
+    acccircle.setRadius(e.accuracy).setLatLng(e.latlng);
+
+    const params = new URLSearchParams({
+        lat: e.latlng.lat,
+        lon: e.latlng.lng
+    });
+    const res = await fetch(`${tides_url}/closest_point_on_thames?${params}`);
+    const data = await res.json();
+    const latlng = new L.LatLng(data[0], data[1])
+    popup_thames
+        .setLatLng(latlng)
+        .setContent("Closest point on thames at " + latlng.toString())
+        .openOn(map);
+}
+
+map.on('locationfound', onLocationFound);
+
+function onLocationError(e) {
+    alert(e.message);
+}
+
+map.on('locationerror', onLocationError);
+
+async function showStations() {
+    const res = await fetch(`${tides_url}/stations`);
+    const data = await res.json();
+    data.forEach(element => {
+        marker = new L.Marker([element["latitude"], element["longitude"]])
+            .bindTooltip(element["name"],
+                {
+                    permanent: true,
+                    direction: "right"
+                }
+            )
+            .addTo(map);
+    });
+}
+
+showStations();
+
+setInterval(() => map.locate({maxZoom: 16}), 10_000);
